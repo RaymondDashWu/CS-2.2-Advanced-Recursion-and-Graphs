267b9b4d52a42852d9791cbd3c1e120c079d3c6c
RaymondDashWu <raymond31670@hotmail.com>
2019-07-13 15:17:01 -0700

    Initial commit

---
+++ challenge1.py
@@ -0,0 +1,10 @@
+# Challenge 1
+# Implement the Graph ADT with an adjacency list
+# Implement code to read in a graph from a text file to create an instance of the Graph ADT and use it's methods.
+# Input: A graph file (can contain a directed or undirected graph with or without weights)
+
+# Output:
+
+# The # vertices in the graph.
+# The # edges in the graph.
+# A list of the edges with their weights (if weighted)
